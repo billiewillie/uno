37cc50592e33022cae80cf430841f2d745e01f1b
vadimbelinovich <vadimbelinovich@gmail.com>
2019-03-05 19:29:43 +0300

    Edits animation

--- js/main.js
+++ js/main.js
@@ -4,11 +4,41 @@
   const search = document.querySelector('.item__search');
   const inputSearch = document.querySelector('.form__search .input');
   const headerContentBottom = document.querySelector('.header__content--bottom');
+  const svg = document.querySelector('.svg');
+  const polygon = document.querySelector('.polygon');
+  let svgHeight = parseInt(getComputedStyle(svg).height);
+  let svgWidth = parseInt(getComputedStyle(svg).width);
 
   search.addEventListener('click', function(e){
     e.preventDefault();
     headerContentBottom.classList.toggle('show');
     inputSearch.focus();
   });
+
+  function getPolygon(){
+    svg.setAttribute('viewBox', '0 0 '+svgWidth+' '+svgHeight);
+    polygon.setAttribute('points', '0,0 '+svgWidth+',0 '+svgWidth+','+svgHeight+' 0,'+svgHeight);
+    return polygon;
+  };
+
+  let coords = getPolygon();
+
+  let x = 0.1;
+  function func(){
+    if(x <= 1){
+      coords = getPolygon();
+      coords = polygon.setAttribute('points', '0,0 '+svgWidth+',0 '+svgWidth+','+svgHeight+' '+(0+svgWidth*x)+','+(svgHeight-svgHeight*x));
+      x += 0.001;
+    }
+    return coords;
+  }
+
+  let svgFadeOut = setInterval(func, 1);
+  // console.log(func());
+    
+  // document.addEventListener('click', svgFadeOut);
+
+  // console.log(coords);
+  
 
 })();
