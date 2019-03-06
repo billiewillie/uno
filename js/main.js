eb7c661fc7a4ccf0628b32cb3f151036cac037f4
vadimbelinovich <vadimbelinovich@gmail.com>
2019-03-06 12:21:07 +0300

    Edits footer

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@
   const headerContentBottom = document.querySelector('.header__content--bottom');
   const svg = document.querySelector('.svg');
   const polygon = document.querySelector('.polygon');
+  const mainFooter = document.querySelector('.main__footer');
   let svgHeight = parseInt(getComputedStyle(svg).height+100);
   let svgWidth = parseInt(getComputedStyle(svg).width+100);
 
@@ -15,34 +16,45 @@
     inputSearch.focus();
   });
 
-  // function getPolygon(){
-  //   svg.setAttribute('viewBox', '0 0 '+svgWidth+' '+svgHeight);
-  //   polygon.setAttribute('points', '0,0 '+svgWidth+',0 '+svgWidth+','+svgHeight+' 0,'+svgHeight);
-  //   return polygon;
-  // };
+  function getPolygon(){
+    svg.setAttribute('viewBox', '0 0 '+svgWidth+' '+svgHeight);
+    polygon.setAttribute('points', '0,0 '+svgWidth+',0 '+svgWidth+','+svgHeight+' 0,'+svgHeight);
+    return polygon;
+  };
 
-  // let coords = getPolygon();
+  let coords = getPolygon();
 
-  // let x = 0.1;
-  // function func(){
-  //   if(x <= 1.1){
-  //     coords = getPolygon();
-  //     coords = polygon.setAttribute('points', '0,0 '+svgWidth+',0 '+svgWidth+','+svgHeight+' '+(0+svgWidth*x)+','+(svgHeight-svgHeight*x));
-  //     x += 0.002;
-  //   }
-  //   return coords;
-  // }
+  let x = 0.1;
+  function func(){
+    if(x <= 1.1){
+      coords = getPolygon();
+      coords = polygon.setAttribute('points', '0,0 '+svgWidth+',0 '+svgWidth+','+svgHeight+' '+(0+svgWidth*x)+','+(svgHeight-svgHeight*x));
+      x += 0.002;
+    }
+    return coords;
+  }
 
-  // document.addEventListener('mousewheel', function(){
+  function showScreen () {
+    setInterval(func, 1);
+    mainFooter.classList.add('show');
+  }
+
+  document.addEventListener('mousewheel', function(){
   
-  //   setInterval(func, 1);
+    showScreen();
 
-  // });
+  });
 
-  // document.addEventListener('touchmove', function(){
+  document.addEventListener('touchmove', function(){
   
-  //   setInterval(func, 1);
+    showScreen();
 
-  // });
+  });
+
+  if(window.innerWidth > window.innerHeight){
+
+    
+
+  }
 
 })();
