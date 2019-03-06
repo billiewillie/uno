0397ac207a3158824de7f4e12535a98818c343d1
vadimbelinovich <vadimbelinovich@gmail.com>
2019-03-06 12:01:14 +0300

    Edits header

--- js/main.js
+++ js/main.js
@@ -6,8 +6,8 @@
   const headerContentBottom = document.querySelector('.header__content--bottom');
   const svg = document.querySelector('.svg');
   const polygon = document.querySelector('.polygon');
-  let svgHeight = parseInt(getComputedStyle(svg).height);
-  let svgWidth = parseInt(getComputedStyle(svg).width);
+  let svgHeight = parseInt(getComputedStyle(svg).height+100);
+  let svgWidth = parseInt(getComputedStyle(svg).width+100);
 
   search.addEventListener('click', function(e){
     e.preventDefault();
@@ -15,34 +15,34 @@
     inputSearch.focus();
   });
 
-  function getPolygon(){
-    svg.setAttribute('viewBox', '0 0 '+svgWidth+' '+svgHeight);
-    polygon.setAttribute('points', '0,0 '+svgWidth+',0 '+svgWidth+','+svgHeight+' 0,'+svgHeight);
-    return polygon;
-  };
+  // function getPolygon(){
+  //   svg.setAttribute('viewBox', '0 0 '+svgWidth+' '+svgHeight);
+  //   polygon.setAttribute('points', '0,0 '+svgWidth+',0 '+svgWidth+','+svgHeight+' 0,'+svgHeight);
+  //   return polygon;
+  // };
 
-  let coords = getPolygon();
+  // let coords = getPolygon();
 
-  let x = 0.1;
-  function func(){
-    if(x <= 1.1){
-      coords = getPolygon();
-      coords = polygon.setAttribute('points', '0,0 '+svgWidth+',0 '+svgWidth+','+svgHeight+' '+(0+svgWidth*x)+','+(svgHeight-svgHeight*x));
-      x += 0.002;
-    }
-    return coords;
-  }
+  // let x = 0.1;
+  // function func(){
+  //   if(x <= 1.1){
+  //     coords = getPolygon();
+  //     coords = polygon.setAttribute('points', '0,0 '+svgWidth+',0 '+svgWidth+','+svgHeight+' '+(0+svgWidth*x)+','+(svgHeight-svgHeight*x));
+  //     x += 0.002;
+  //   }
+  //   return coords;
+  // }
 
-  document.addEventListener('mousewheel', function(){
+  // document.addEventListener('mousewheel', function(){
   
-    setInterval(func, 1);
+  //   setInterval(func, 1);
 
-  });
+  // });
 
-  document.addEventListener('touchmove', function(){
+  // document.addEventListener('touchmove', function(){
   
-    setInterval(func, 1);
+  //   setInterval(func, 1);
 
-  });
+  // });
 
 })();
