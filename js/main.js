94d5b70627c57fb8b19cbbfdb3d1fa98ee5bca37
vadimbelinovich <vadimbelinovich@gmail.com>
2019-04-23 19:50:23 +0300

    edited

--- js/main.js
+++ js/main.js
@@ -124,6 +124,7 @@
   const inputPhone = document.querySelector('.input__phone');
   const inputEmail = document.querySelector('.input__email');
   const userUrl = document.querySelector('.user_url');
+  const tabgroup = document.querySelector('.tabgroup');
 
   // about selection
   $('.about__title--item').on('click', function (e) {
@@ -270,5 +271,21 @@
       }
     })
   });
+
+  if(tabgroup){
+    $('.tabgroup > div').hide();
+    $('.tabgroup > div:first-of-type').show();
+    $('.tabs a').click(function(e){
+      e.preventDefault();
+      var $this = $(this),
+          tabgroup = '#'+$this.parents('.tabs').data('tabgroup'),
+          others = $this.closest('li').siblings().children('a'),
+          target = $this.attr('href');
+      others.removeClass('active');
+      $this.addClass('active');
+      $(tabgroup).children('div').hide();
+      $(target).show();
+    });
+  }
   
 })();
